cc46bd4696e687ce0c9911d6205890fa2bf0b8f7
TeaCatChou <41939574+TeaCatChou@users.noreply.github.com>
2018-11-28 16:33:11 +0800

    更新 8個Controller & View 新版

--- ProjectManager/Controllers/DepartmentController.cs
+++ ProjectManager/Controllers/DepartmentController.cs
@@ -27,7 +27,7 @@ namespace ProjectManager.Controllers
                     _department.ParentDepartmentGUID = new Guid(Request.Form["ParentDepartmentGUID"]);
                 }
                 _department.DepartmentGUID = Guid.NewGuid();
-                _department.DepartmentName = Request.Form["DepartmentName"];              
+                _department.DepartmentName = Request.Form["DepartmentName"];
                 depRepository.Add(_department);
                 return RedirectToAction("Index");
             }
