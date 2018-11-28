1d1da096fa18d444108df9bf1231d2838751b7f3
LesleyChang <lesleychang1024@gmail.com>
2018-11-28 17:48:59 +0800

    Completed Edit project
    完成專案修改，儲存session["ProjectGUID"]

--- ProjectManager/Controllers/ProjectDetailsController.cs
+++ ProjectManager/Controllers/ProjectDetailsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,46 @@ namespace ProjectManager.Controllers
 {
     public class ProjectDetailsController : Controller
     {
+        Repository<Project> projectRepo = new Repository<Project>();
         // GET: ProjectDetails
+
         public ActionResult ProjectReport(Guid? ProjectGUID)
         {
+            if (ProjectGUID != null)
+            {
+                Session["ProjectGUID"] = ProjectGUID;
+            }
             return View();
+        }
+        [HttpGet]
+        public ActionResult ProjectEdit()
+        {
+            if (Session["ProjectGUID"] == null)
+                return RedirectToAction("Index", "Projects");
+
+            ViewBag.Departments = new SelectList(new Repository<Department>().GetCollections(), "DepartmentGUID", "DepartmentName");
+            ViewBag.Employees = new SelectList(new Repository<Employee>().GetCollections(), "EmployeeGUID", "EmployeeName");
+            ViewBag.ProjectStatuses = new SelectList(new Repository<ProjectStatus>().GetCollections(), "ProjectStatusID", "ProjectStatusName");
+            ViewBag.ProjectCategories = new SelectList(new Repository<ProjectCategory>().GetCollections(), "ProjectCategoryID", "ProjectCategoryName");
+            Guid _projectGUID = new Guid(Session["ProjectGUID"].ToString());            
+            return View(projectRepo.Find(_projectGUID));
+        }
+        [HttpPost]
+        public ActionResult ProjectEdit(Project _project)
+        {
+            var recentProject = projectRepo.Find(_project.ProjectGUID);
+            recentProject.ProjectName = _project.ProjectName;
+            recentProject.ProjectStatusID = _project.ProjectStatusID;
+            recentProject.ProjectCategoryID = _project.ProjectCategoryID;
+            recentProject.ProjectSupervisorGUID = _project.ProjectSupervisorGUID;
+            recentProject.EstStartDate = _project.EstStartDate;
+            recentProject.EstEndDate = _project.EstEndDate;
+            recentProject.StartDate = _project.StartDate;
+            recentProject.Description = _project.Description;
+            recentProject.IsGeneralManagerConcerned = _project.IsGeneralManagerConcerned;
+
+            projectRepo.Update(recentProject);
+            return ProjectEdit();
         }
     }
 }
--- ProjectManager/Models/BLL/ProjectsBL.cs
+++ ProjectManager/Models/BLL/ProjectsBL.cs
@@ -7,6 +7,11 @@ namespace ProjectManager.Models
 {
     public static class ProjectsBL
     {
+        /// <summary>
+        /// 回傳去掉開頭P的最後一個專案編號
+        /// </summary>
+        /// <param name="projects"></param>
+        /// <returns></returns>
         public static int GetLastProjectIntID(this IEnumerable<Project> projects)
         {
             var q = projects.Select(n => n.ProjectID);
@@ -16,6 +21,18 @@ namespace ProjectManager.Models
                 ProjectIntID.Add(int.Parse(x.Substring(1, 5)));
             }
             return ProjectIntID.Max();
+        }
+        /// <summary>
+        /// 回傳List&lt;GroupedProject>，將傳入值GroupBy(requiredDeptName)
+        /// </summary>
+        /// <param name="projects"></param>
+        /// <returns></returns>
+        public static IEnumerable<GroupedProject> GetGroupedProject(this IEnumerable<Project> projects)
+        {
+            return projects.AsEnumerable()
+                .OrderBy(p => p.ProjectID)
+                .GroupBy(p => p.Department.DepartmentName)
+                .Select(g => new GroupedProject() { DepartmentName = g.Key, group = g }).ToList();
         }
     }
 }
