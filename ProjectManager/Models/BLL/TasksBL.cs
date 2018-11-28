8a412f90d365ce5d114db9e96df143ef3e67f786
LesleyChang <lesleychang1024@gmail.com>
2018-11-29 00:23:28 +0800

    Completed insert new Task
    完成Task新增功能，將nav改至Shared資料夾_PartialPageNav.cshtml

--- ProjectManager/Models/BLL/TasksBL.cs
+++ ProjectManager/Models/BLL/TasksBL.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ProjectManager.Models.BLL
+namespace ProjectManager.Models
 {
     public static class TasksBL
     {
