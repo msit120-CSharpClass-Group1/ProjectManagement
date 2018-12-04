2aae5205c9bcbfba6c3869355e1467dd7d4207ae
dks50217 <dks50217@gmail.com>
2018-12-04 13:01:53 +0800

    增加專案評分導覽列及入口介面

--- ProjectManager/Controllers/PerfomanceController.cs
+++ ProjectManager/Controllers/PerfomanceController.cs
@@ -10,10 +10,12 @@ namespace ProjectManager.Controllers
     public class PerfomanceController : Controller
     {
         Repository<Project> projectRepo = new Repository<Project>();
+        Repository<ProjectMembers> ProjectMembersRepo = new Repository<ProjectMembers>();
         public ActionResult Index()
         {
             ViewBag.InProgress = projectRepo.GetCollections().Where(p => p.ProjectStatusID == 1).ToList();
-            ViewBag.Closed = projectRepo.GetCollections().Where(p => p.ProjectStatusID == 3).ToList();
+            ViewBag.Closed = projectRepo.GetCollections().Where(p => p.ProjectStatusID == 2).ToList();
+            ViewBag.GroupMember = ProjectMembersRepo.GetCollections().GroupBy(p => p.Project.ProjectName).ToList();      
             return View();
         }
     }
