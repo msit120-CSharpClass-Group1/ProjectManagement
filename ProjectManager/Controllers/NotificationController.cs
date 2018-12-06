fd5ff10aacda22cf9f73815040bb81f27dfbb467
a615125340 <a615125340@yahoo.com.tw>
2018-12-06 17:31:32 +0800

    Notification

---
+++ ProjectManager/Controllers/NotificationController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManager.Models;
+
+namespace ProjectManager.Controllers
+{
+    [Authorize]
+    public class NotificationController : Controller
+    {
+        IRepository<Tasks> tasks = new Repository<Tasks>();
+        IRepository<Members> members = new Repository<Members>();
+        // GET: Notification
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Load()
+        {
+            var _members =members.Find(new Guid(Request.Cookies["MemberGUID"].Value));
+            var _tasks =tasks.GetCollections().Where(n => n.EmployeeGUID == _members.EmployeeGUID&&n.IsRead==false).Select(n=>new { n.TaskName,n.AssignedDate});
+            return Json(_tasks);
+        }
+    }
+}
--- ProjectManager/Controllers/ProjectDetailsController.cs
+++ ProjectManager/Controllers/ProjectDetailsController.cs
@@ -271,8 +271,6 @@ namespace ProjectManager.Controllers
             task.ProjectGUID = _projectGUID;
             task.TaskGUID = Guid.NewGuid();
             task.EstWorkTime = task.GetEstWorkTime(System.Web.HttpContext.Current.Application["Holidays"] as HolidaysVM);
-            task.StartDate = task.EstStartDate;
-            task.EndDate = task.EstEndDate;
             taskRepo.Add(task);
             return Json("success", JsonRequestBehavior.AllowGet);
             //return RedirectToAction("ProjectDistribution");
@@ -292,10 +290,11 @@ namespace ProjectManager.Controllers
             recentTask.Tag = taskModified.Tag;
             recentTask.EstStartDate = taskModified.EstStartDate;
             recentTask.EstEndDate = taskModified.EstEndDate;
-            recentTask.StartDate = taskModified.EstStartDate;
+            recentTask.StartDate = taskModified.StartDate;
+            recentTask.EndDate = taskModified.EndDate;
             recentTask.Description = taskModified.Description;
             recentTask.EstWorkTime = taskModified.GetEstWorkTime(System.Web.HttpContext.Current.Application["Holidays"] as HolidaysVM);
-           
+
             taskRepo.Update(recentTask);            
             return Json("success", JsonRequestBehavior.AllowGet);
             //return RedirectToAction("ProjectDistribution");
@@ -338,7 +337,7 @@ namespace ProjectManager.Controllers
         [HttpPost]
         public ActionResult LoadHolidays(HolidaysVM holidays)
         {
-            Session["Holidays"] = "loaded";
+            //Session["Holidays"] = holidays;
             System.Web.HttpContext.Current.Application.Lock();
             System.Web.HttpContext.Current.Application["Holidays"] = holidays;
             System.Web.HttpContext.Current.Application.UnLock();
