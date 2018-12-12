1f0156e97af182c49c848910a59f17f29b55ed72
TeaCatChou <41939574+TeaCatChou@users.noreply.github.com>
2018-12-12 21:08:31 +0800

    聊天室雛形

---
+++ ProjectManager/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ProjectManager.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string nickName, string message)  //給前端呼叫使用
+        {
+            Clients.All.gotMessage(nickName, message);
+        }
+    }
+}
