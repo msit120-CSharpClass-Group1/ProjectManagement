293db4af134b1b8c958c5bec076507a4b6abf56f
Anna <chiyang9925119.@gmail.com>
2018-12-13 19:54:57 +0800

    bread

--- ProjectManager/Controllers/SelectMBoardController.cs
+++ ProjectManager/Controllers/SelectMBoardController.cs
@@ -8,11 +8,11 @@ using System.Web.Mvc;
 namespace ProjectManager.Controllers
 {
     [Authorize]
-    public class SelectMBoarController : Controller
+    public class SelectMBoardController : Controller
     {
         Repository<ProjectManager.Models.ProjectMembers> pm = new Repository<ProjectManager.Models.ProjectMembers>();
         Repository<ProjectManager.Models.Project> p = new Repository<ProjectManager.Models.Project>();
-        // GET: SelectMBoar
+        // GET: SelectMBoard
         public ActionResult Index(Guid id)
         {
             var MenberBoard = pm.GetCollections().Where(x => x.ProjectGUID == id);
