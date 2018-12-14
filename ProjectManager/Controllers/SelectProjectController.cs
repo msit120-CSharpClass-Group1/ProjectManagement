2516ad965091e504dce8735aeb10c766f04cb3ee
Anna <chiyang9925119.@gmail.com>
2018-12-14 18:41:13 +0800

    title button bug

--- ProjectManager/Controllers/SelectProjectController.cs
+++ ProjectManager/Controllers/SelectProjectController.cs
@@ -13,13 +13,17 @@ namespace ProjectManager.Controllers
     {
         Repository<ProjectManager.Models.ProjectMembers> pm = new Repository<ProjectManager.Models.ProjectMembers>();
         Repository<ProjectManager.Models.Members> m = new Repository<ProjectManager.Models.Members>();
+        Repository<ProjectManager.Models.Project> p = new Repository<ProjectManager.Models.Project>();
 
         // GET: SelectProject
         public ActionResult Index()
         {
+            BoardVM VM = new BoardVM();
             var members = m.Find(new Guid(Request.Cookies["MemberGUID"].Value));
-            var ProjectList = pm.GetCollections().Where(x=>x.EmployeeGUID==members.EmployeeGUID);
-            return View(ProjectList);
+            //VM.GetProjectDepartment = pm.GetCollections().Where(x => x.EmployeeGUID == members.EmployeeGUID && x.Project.ProjectStatusID == 1).GroupBy(x=>x.Project.Department.DepartmentName).Select(g=>new )
+            VM.ProjectMember = pm.GetCollections().Where(x => x.EmployeeGUID == members.EmployeeGUID && x.Project.ProjectStatusID == 1).ToList();
+            //var ProjectList = pm.GetCollections().Where(x=>x.EmployeeGUID==members.EmployeeGUID && x.Project.ProjectStatusID == 1).ToList();
+            return View(VM);
         }
 
         
