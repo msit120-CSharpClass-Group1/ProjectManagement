c9e33e02a88fbe86129f5b4777105ab06781a13a
dks50217 <dks50217@gmail.com>
2018-12-17 17:29:51 +0800

    修正多項BUG、新增欄位、更新EF

--- ProjectManager/Models/Department.cs
+++ ProjectManager/Models/Department.cs
@@ -9,7 +9,6 @@
 
 namespace ProjectManager.Models
 {
-    using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
     
@@ -29,6 +28,7 @@ namespace ProjectManager.Models
         public int DepartmentID { get; set; }
         public string DepartmentName { get; set; }
         public Nullable<System.Guid> ParentDepartmentGUID { get; set; }
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Department> Department1 { get; set; }
         public virtual Department Department2 { get; set; }
